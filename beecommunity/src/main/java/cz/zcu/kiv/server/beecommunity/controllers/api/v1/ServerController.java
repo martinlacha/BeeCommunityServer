c21e332226d051212d0f07e502dabe88079f48d4
Martin Lacha <martin.lacha@unicorn.com>
2024-02-08 15:20:29 +0100

    Add endpoint to test connection with server

---
+++ beecommunity/src/main/java/cz/zcu/kiv/server/beecommunity/controllers/api/v1/ServerController.java
@@ -0,0 +1,26 @@
+package cz.zcu.kiv.server.beecommunity.controllers.api.v1;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/server")
+@Tag(name = "Server")
+public class ServerController {
+
+    /**
+     * Endpoint to test is connection is established
+     * @return HttpStatus.OK if connection is established
+     */
+    @GetMapping("/test-connection")
+    ResponseEntity<Void> testConnection() {
+        log.info("Successful connection test");
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
