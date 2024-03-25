196e974ed5195cbf27df6c013846131a50e7b0d0
Martin Lacha <martin.lacha@unicorn.com>
2024-03-25 14:55:51 +0100

    Scheduling task for database backup

--- beecommunity/src/main/java/cz/zcu/kiv/server/beecommunity/BeeCommunityApplication.java
+++ beecommunity/src/main/java/cz/zcu/kiv/server/beecommunity/BeeCommunityApplication.java
@@ -4,10 +4,13 @@ import cz.zcu.kiv.server.beecommunity.config.PropertiesConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Main function and entry point where start spring application
  */
+
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties(PropertiesConfiguration.class)
 public class BeeCommunityApplication {
