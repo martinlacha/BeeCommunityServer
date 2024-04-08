3fee5a6679641dc533d4b33d09bd549cde782975
Martin Lacha <martin.lacha@unicorn.com>
2024-04-08 11:59:07 +0200

    Fix json decentralize, change user default passwords

--- beecommunity/src/main/java/cz/zcu/kiv/server/beecommunity/jpa/dto/user/UserRolesDto.java
+++ beecommunity/src/main/java/cz/zcu/kiv/server/beecommunity/jpa/dto/user/UserRolesDto.java
@@ -1,12 +1,16 @@
 package cz.zcu.kiv.server.beecommunity.jpa.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Dto for admin page where admin can grant and revoke admin role and ban users
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UserRolesDto {
     Long userId;
--- beecommunity/src/main/resources/db/migration/V01.02__insert_test_data.sql
+++ beecommunity/src/main/resources/db/migration/V01.02__insert_test_data.sql
@@ -6,6 +6,8 @@ INSERT into ADDRESS(state, country, town, street, house_number)
     values('Gernamy', 'Gernamy', 'Passau', 'Street', 4);
 INSERT into ADDRESS(state, country, town, street, house_number)
     values('Czech Republic', 'Czech Republic', 'Chlumany', 'none', 123);
+INSERT into ADDRESS(state, country, town, street, house_number)
+values('', '', '', '', 1);
 
 INSERT into USER_INFO(name, surname, date_of_birth, address_id)
     values ('Martin', 'Lácha', CURRENT_DATE, 1);
@@ -15,20 +17,22 @@ INSERT into USER_INFO(name, surname, date_of_birth, address_id)
     values ('Jan', 'Lácha', CURRENT_DATE, 3);
 INSERT into USER_INFO(name, surname, date_of_birth, address_id)
     values ('Charon', 'Janeček', CURRENT_DATE, 4);
+INSERT into USER_INFO(name, surname, date_of_birth, address_id)
+values ('Admin', '', CURRENT_DATE, 5);
 
 INSERT into AUTH_ROLE (role_name) values ('ADMIN');
 INSERT into AUTH_ROLE (role_name) values ('USER');
 
 INSERT into AUTH_USER(email, password, user_info_id)
-    values('martin.lacha@seznam.cz', '$2a$10$KA2xCDfloVWE8pSyjiIx5eg8f6v7KGUqRWNu6yEVA7.OtBWPuMAL2', 1);
+values('admin@seznam.cz', '$2a$10$w8vrvSO5DOCuyMXoI3KHieiBrTf4YQ3.GEeaVGC/ynBtiK3vNdgsu', 5);
 INSERT into AUTH_USER(email, password, user_info_id)
-    values('vlastimil.lacha@gmail.com', 'password', 2);
+    values('martin.lacha@seznam.cz', '$2a$10$w8vrvSO5DOCuyMXoI3KHieiBrTf4YQ3.GEeaVGC/ynBtiK3vNdgsu', 1);
 INSERT into AUTH_USER(email, password, user_info_id)
-    values('random_email@seznam.cz', '$2a$10$KA2xCDfloVWE8pSyjiIx5eg8f6v7KGUqRWNu6yEVA7.OtBWPuMAL2', 3);
+    values('vlastimil.lacha@gmail.com', '$2a$10$w8vrvSO5DOCuyMXoI3KHieiBrTf4YQ3.GEeaVGC/ynBtiK3vNdgsu', 2);
 INSERT into AUTH_USER(email, password, user_info_id)
-    values('temporaryAccount@gmail.com', 'password', 4);
+    values('random_email@seznam.cz', '$2a$10$w8vrvSO5DOCuyMXoI3KHieiBrTf4YQ3.GEeaVGC/ynBtiK3vNdgsu', 3);
 INSERT into AUTH_USER(email, password, user_info_id)
-    values('n@s', '$2a$10$1Ubym8iB9sRrXU36JC9etun11siJ4/qURUhYfM2t4.9j9beReewxS', 2);
+    values('testing@gmail.com', '$2a$10$w8vrvSO5DOCuyMXoI3KHieiBrTf4YQ3.GEeaVGC/ynBtiK3vNdgsu', 4);
 
 INSERT INTO AUTH_USER_ROLE (user_id, role_id) VALUES (1, 1);
 INSERT INTO AUTH_USER_ROLE (user_id, role_id) VALUES (1, 2);
