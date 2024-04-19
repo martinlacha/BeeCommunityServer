e262b7eff72ad38bee57fd1bfb4c2aea1757447d
Martin Lacha <martin.lacha@unicorn.com>
2024-04-19 11:48:54 +0200

    Add unit tests and ignore list

---
+++ beecommunity/src/test/java/cz/zcu/kiv/server/beecommunity/BeeCommunityApplicationTest.java
@@ -0,0 +1,11 @@
+package cz.zcu.kiv.server.beecommunity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeeCommunityApplicationTest {
+
+    @Test
+    void main() {}
+}
